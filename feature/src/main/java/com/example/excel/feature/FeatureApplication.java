ae5f47471d759558ace69a4a36d28e53b493a726
lkwoung88 <lkwoung88@gmail.com>
2023-10-23 23:24:45 +0900

    init : feature project upload

---
+++ feature/src/main/java/com/example/excel/feature/FeatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.excel.feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureApplication.class, args);
+	}
+
+}
