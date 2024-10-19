f5177552d039613ab072aa80f6aae047928116c7
lkwoung88 <lkwoung88@gmail.com>
2024-10-20 02:05:54 +0900

    [feat] 엑셀 업로드 - 리펙터링

--- feature/src/main/java/com/feature/excel/object/ExcelImporter.java
+++ feature/src/main/java/com/feature/excel/object/ExcelImporter.java
@@ -5,6 +5,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.springframework.lang.Nullable;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -51,107 +52,114 @@ public abstract class ExcelImporter <T> {
 
             T entity = this.clazz.getDeclaredConstructor().newInstance();
 
-            setFieldValue(columnNum, valueRow, headerRow, entity);
+            setFieldsValue(columnNum, valueRow, headerRow, entity);
 
             this.entities.add(entity);
         }
 
     }
 
-    private void setFieldValue(int columnNum, Row valueRow, Row headerRow, T entity) throws IllegalAccessException {
+    protected abstract int saveEntities();
+
+    private void setFieldsValue(int columnNum, Row valueRow, Row headerRow, T entity) throws IllegalAccessException {
 
         for (int cellIndex = 0; cellIndex < columnNum; cellIndex++) {
 
-            String cellValue = "";
-            Cell cell = valueRow.getCell(cellIndex);
-
-            if (cell == null) {
-                continue;
-            }
-
-            CellType cellType = cell.getCellType();
-
-            if ("NUMERIC".equals(cellType.toString())) {
-
-                cellValue = String.valueOf((int)cell.getNumericCellValue());
-            } else if ("STRING".equals(cellType.toString())) {
-
-                cellValue = cell.getStringCellValue();
-            } else {
-
-                continue;
-            }
-
+            String cellValue = setCellValue(valueRow, cellIndex);
             String headerName = headerRow.getCell(cellIndex).getStringCellValue();
-            Field entityField = null;
+            Field entityField = getEntityField(headerName);
 
-            Field[] fields = clazz.getDeclaredFields();
+            setFieldValue(entity, entityField, cellValue);
+        }
+    }
 
-            for (Field field : fields) {
+    private <T> void setFieldValue(T entity, Field entityField, String cellValue) throws IllegalAccessException {
+        if (entityField != null) {
+            try {
+                entityField.setAccessible(true);
 
-                if (field.isAnnotationPresent(ExcelHeader.class)) {
+                if (entityField.getType() == String.class) {
 
-                    ExcelHeader excelHeader = field.getAnnotation(ExcelHeader.class);
+                    entityField.set(entity, cellValue);
+                }
+                else if (entityField.getType() == short.class) {
 
-                    if (excelHeader.name().equals(headerName)) {
+                    entityField.set(entity, Short.parseShort(cellValue));
+                }
+                else if (entityField.getType() == int.class) {
 
-                        entityField = field;
-                    }
+                    String[] split = cellValue.split("\\.");
+                    entityField.set(entity, Integer.parseInt(split[0]));
+                }
+                else if (entityField.getType() == long.class) {
+
+                    entityField.set(entity, Long.parseLong(cellValue));
+                }
+                else if (entityField.getType() == float.class) {
+
+                    entityField.set(entity, Float.parseFloat(cellValue));
+                }
+                else if (entityField.getType() == double.class) {
+
+                    entityField.set(entity, Double.parseDouble(cellValue));
+                }
+                else if (entityField.getType() == boolean.class) {
+
+                    entityField.set(entity, Boolean.parseBoolean(cellValue));
+                }
+                else if (entityField.getType() == char.class) {
+
+                    entityField.set(entity, cellValue);
+                }
+                else {
+
                 }
             }
+            catch (NumberFormatException e) {
 
-            if (entityField != null) {
-                try {
-                    entityField.setAccessible(true);
+                log.error(e.getMessage());
+            }
+            finally {
 
-                    if (entityField.getType() == String.class) {
-
-                        entityField.set(entity, cellValue);
-                    }
-                    else if (entityField.getType() == short.class) {
-
-                        entityField.set(entity, Short.parseShort(cellValue));
-                    }
-                    else if (entityField.getType() == int.class) {
-
-                        String[] split = cellValue.split("\\.");
-                        entityField.set(entity, Integer.parseInt(split[0]));
-                    }
-                    else if (entityField.getType() == long.class) {
-
-                        entityField.set(entity, Long.parseLong(cellValue));
-                    }
-                    else if (entityField.getType() == float.class) {
-
-                        entityField.set(entity, Float.parseFloat(cellValue));
-                    }
-                    else if (entityField.getType() == double.class) {
-
-                        entityField.set(entity, Double.parseDouble(cellValue));
-                    }
-                    else if (entityField.getType() == boolean.class) {
-
-                        entityField.set(entity, Boolean.parseBoolean(cellValue));
-                    }
-                    else if (entityField.getType() == char.class) {
-
-                        entityField.set(entity, cellValue);
-                    }
-                    else {
-
-                    }
-                }
-                catch (NumberFormatException e) {
-
-                    log.error(e.getMessage());
-                }
-                finally {
-
-                    entityField.setAccessible(false);
-                }
+                entityField.setAccessible(false);
             }
         }
     }
 
-    protected abstract int saveEntities();
+    @Nullable
+    private String setCellValue(Row valueRow, int cellIndex) {
+
+        Cell cell = valueRow.getCell(cellIndex);
+        CellType cellType = cell.getCellType();
+
+        if ("NUMERIC".equals(cellType.toString())) {
+
+            return String.valueOf((int)cell.getNumericCellValue());
+        } else if ("STRING".equals(cellType.toString())) {
+
+            return cell.getStringCellValue();
+        } else {
+
+            return null;
+        }
+    }
+
+    @Nullable
+    private Field getEntityField(String headerName) {
+        Field[] fields = clazz.getDeclaredFields();
+
+        for (Field field : fields) {
+
+            if (field.isAnnotationPresent(ExcelHeader.class)) {
+
+                ExcelHeader excelHeader = field.getAnnotation(ExcelHeader.class);
+
+                if (excelHeader.name().equals(headerName)) {
+
+                    return field;
+                }
+            }
+        }
+        return null;
+    }
 }
